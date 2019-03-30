b9c9cee794486429b498272f0c576cfc76fe4aea
Kamil Maka <maka.kamil@gmail.com>
2019-03-30 15:30:53 +0100

    Adding addresses details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_141021) do
+ActiveRecord::Schema.define(version: 2019_03_30_142103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2019_03_30_141021) do
   create_table "addresses", force: :cascade do |t|
     t.string "addressable_type"
     t.bigint "addressable_id"
+    t.string "city"
+    t.string "street_name"
+    t.string "street_no"
+    t.string "postal_code"
     t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id"
   end
 
