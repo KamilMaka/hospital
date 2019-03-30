d476cc125d40169f80ec72169915ee2c358f33e4
Kamil Maka <maka.kamil@gmail.com>
2019-03-30 13:31:18 +0100

    Adding scaffold for Reservation, some model changes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,12 @@ class Patient < ApplicationRecord
   validates_presence_of :name, :surname, :pesel
   validates :pesel, format: { with: /\d{11}/,
                               message: "should contain 11 digits" }
+
+  def surname_and_name
+    "#{surname} #{name}"
+  end
+
+  def to_s
+    surname_and_name
+  end
 end
